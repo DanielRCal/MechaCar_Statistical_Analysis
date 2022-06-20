36ed84ceb06ba24b6a609ebe4106603c4b04f981
DanielC <drcal@Daniels-MBP.attlocal.net>
2022-06-20 03:10:55 -0500

    Deliverable 1- Technical Analysis Submit

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,14 @@
+#Deliverable 1: Linear Regression to Predict MPG
+#Import packages
+library(dplyr)
+library(tidyverse)
+
+#Import and Read the CSV file
+car_Data <- read.csv('MechaCar_mpg.csv')
+head(car_Data)
+
+#perform multiple linear regression model
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data =car_Data)
+
+#generate summary statistics
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data =car_Data)) 
