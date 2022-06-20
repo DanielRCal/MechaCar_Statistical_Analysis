05104bc847caf7e409c9317548858bce2d95aca5
DanielC <drcal@Daniels-MBP.attlocal.net>
2022-06-20 03:42:31 -0500

    Deliverable 2 - Creation of Visualizations for Trip Anaylysis

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,4 +1,5 @@
 #Deliverable 1: Linear Regression to Predict MPG
+
 #Import packages
 library(dplyr)
 library(tidyverse)
@@ -12,3 +13,21 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #determine summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data =car_Data)) 
+
+#Deliverable 2: Create Visualizations for the Trip Analysis
+
+#Import and Read the CSV File
+coil_Data <- read.csv('Suspension_Coil.csv')
+head(coil_Data)
+
+#mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+Mean = mean(coil_Data$PSI)
+Median=median(coil_Data$PSI)
+Variance=var(coil_Data$PSI)
+SD = sd(coil_Data$PSI)
+
+#Total_Summary Data Frame
+total_Summary <- data.frame(Mean,Median,Variance,SD)
+
+#summary by lot
+lot_summary <- coil_Data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI), .groups = 'keep') 
--- README.md
+++ README.md
@@ -6,3 +6,6 @@ The variables/ coefficients which provided a non-random amount of variance to th
 The slope of the linear model is not considered to be zero because the value of p is less than zero.
 
 This linear model can be used to  predict mpg of MechaCar prototypes with effectiveness of roughly 71.4%.
+
+## Summary Statistics on Suspention Coils
+Current manufacturing data indicates that the total design specifications for the MechaCar suspension coils are not being met in total.  Individual Lots 1 and 2 meet design specifications. However, Lot 3 exceeds the variance limitations of design specification.
